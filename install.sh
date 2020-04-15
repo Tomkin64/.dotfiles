0e7d7bb3b20c68464053c9cf3c33b66c0169bf65
Tomkin64 <tom@tomkadlec.cz>
2020-04-15 20:01:42 +0200

    added install htop + strace modified mkdir for ~/.cache/zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt install tmux vim lnav curl git zsh --yes
-mkdir ~/.cache/zsh
+sudo apt install tmux vim lnav curl git zsh htop strace --yes
+mkdir -p ~/.cache/zsh
 touch ~/.cache/zsh/history
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux-conf ~/.tmux.conf
@@ -9,3 +9,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 rm -f ~/.vimrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
+chsh -s /usr/bin/zsh
