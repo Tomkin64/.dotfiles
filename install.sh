82b4ba968131b4345c14c633237d4aaca5fcb1aa
Tomkin64 <47191843+Tomkin64@users.noreply.github.com>
2023-03-02 13:39:48 +0100

    Migration from Vundle to vim-plug

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ touch ~/.cache/zsh/history
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux-conf ~/.tmux.conf
 ln -s ~/.dotfiles/vimrc-install ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 rm -f ~/.vimrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 chsh -s /usr/bin/zsh
